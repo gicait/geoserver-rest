910d3bbf63846370769777940b7d5906ad8aef90
iamtekson <teksondada@gmail.com>
2021-01-05 16:06:52 +0700

    style name parameter in uploadStyle function added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="geoserver-rest",
-    version="1.2.3",
+    version="1.2.5",
     author="Tek Kshetri",
     author_email="iamtekson@gmail.com",
     description="Package for GeoServer rest API",

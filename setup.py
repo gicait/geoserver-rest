78e35b95739fb1d5ea7f140242c725b45e9d7fb2
iamtekson <teksondada@gmail.com>
2021-03-09 13:47:45 +0700

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="geoserver-rest",
-    version="1.4.0",
+    version="1.5.0",
     author="Tek Kshetri",
     author_email="iamtekson@gmail.com",
     description="Package for GeoServer rest API",

4dd8d4ff4eeaeb1690faf13ddb18fbed7fd6def2
iamtekson <teksondada@gmail.com>
2021-03-25 08:23:32 +0700

    joss section removed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="geoserver-rest",
-    version="1.5.5",
+    version="1.5.6",
     author="Tek Kshetri",
     author_email="iamtekson@gmail.com",
     description="Package for GeoServer rest API",

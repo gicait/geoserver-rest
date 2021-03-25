e4929e2854b3650a3f1c324515924d13832780b5
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2021-03-25 17:06:26 -0400

    Add pre-commit, add setup.cfg with black preferences, reformat setup.py to read from requirements*.txt, store author and version in src files

--- setup.py
+++ setup.py
@@ -1,33 +1,50 @@
-import setuptools
+from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = dict()
+with open(os.path.join(here, "geo", "__version__.py"), "r") as f:
+    exec(f.read(), about)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+reqs = [line.strip() for line in open("requirements.txt")]
+dev_reqs = [line.strip() for line in open("requirements_dev.txt")]
+
+setup(
     name="geoserver-rest",
-    version="1.5.5",
-    author="Tek Kshetri",
-    author_email="iamtekson@gmail.com",
+    version=about["__version__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
     description="Package for GeoServer rest API",
-    py_modules=['geoserver-rest-python'],
+    py_modules=["geoserver-rest-python"],
     # package_dir={'':'src'},
+    license="MIT License",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/iamtekson/geoserver-rest-python",
-    packages=['geo'],
-    keywords=['geoserver-rest-python', 'geoserver rest', 'python geoserver', 'geoserver api',
-              'api', 'rest geoserver', 'python', 'geoserver python', 'geoserver rest'],
+    packages=["geo"],
+    keywords=[
+        "geoserver-rest-python",
+        "geoserver rest",
+        "python geoserver",
+        "geoserver api",
+        "api",
+        "rest geoserver",
+        "python",
+        "geoserver python",
+        "geoserver rest",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'pygments',
-        'pycurl',
-        'seaborn',
-        'gdal',
-        'psycopg2',
-    ],
-    python_requires='>=3.6',
+    install_requires=reqs,
+    extras_require={"dev": dev_reqs},
+    python_requires=">=3.6",
 )
